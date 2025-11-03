270aff24edbf6af5680ef22769430926827920a5
= <joseph.bradshaw@outlook.com>
2025-11-03 21:24:07 +0100

    sides of windows are draggable

--- src/lib/window/Window.svelte
+++ src/lib/window/Window.svelte
@@ -35,11 +35,13 @@
 	let windowElement: HTMLDivElement;
 	let windowBarElement: HTMLDivElement;
 	let topRightHandle: HTMLDivElement;
+	let rightHandle: HTMLDivElement;
+	let leftHandle: HTMLDivElement;
+	let downHandle: HTMLDivElement;
+	let upHandle: HTMLDivElement;
 	let topLeftHandle: HTMLDivElement;
 	let bottomRightHandle: HTMLDivElement;
 	let bottomLeftHandle: HTMLDivElement;
-
-	let proxies = [1, 2, 3, 4].map((_) => document.createElement('div'));
 
 	function updateBottomRightDrag(this: Draggable) {
 		gsap.set(windowElement, { width: '+=' + this.deltaX });
@@ -61,7 +63,25 @@
 		gsap.set(windowElement, { height: '-=' + this.deltaY, y: '+=' + this.deltaY });
 	}
 
+	function updateRightDrag(this: Draggable) {
+		gsap.set(windowElement, { width: '+=' + this.deltaX });
+	}
+
+	function updateLeftDrag(this: Draggable) {
+		gsap.set(windowElement, { width: '-=' + this.deltaX, x: '+=' + this.deltaX });
+	}
+
+	function updateDownDrag(this: Draggable) {
+		gsap.set(windowElement, { height: '+=' + this.deltaY });
+	}
+
+	function updateUpDrag(this: Draggable) {
+		gsap.set(windowElement, { height: '-=' + this.deltaY, y: '+=' + this.deltaY });
+	}
+
 	onMount(() => {
+		const proxies = [1, 2, 3, 4, 5, 6, 7, 8].map(() => document.createElement('div'));
+
 		makeDraggable({ element: windowElement, trigger: windowBarElement });
 
 		Draggable.create(proxies[0], {
@@ -87,39 +107,74 @@
 			cursor: 'nw-resize',
 			onDrag: updateTopLeftDrag
 		});
+
+		Draggable.create(proxies[4], {
+			trigger: rightHandle,
+			cursor: 'e-resize',
+			onDrag: updateRightDrag
+		});
+
+		Draggable.create(proxies[5], {
+			trigger: leftHandle,
+			cursor: 'w-resize',
+			onDrag: updateLeftDrag
+		});
+
+		Draggable.create(proxies[6], {
+			trigger: downHandle,
+			cursor: 's-resize',
+			onDrag: updateDownDrag
+		});
+
+		Draggable.create(proxies[7], {
+			trigger: upHandle,
+			cursor: 'n-resize',
+			onDrag: updateUpDrag
+		});
 	});
 </script>
 
-<div bind:this={windowElement} class="window" style="height: {height}px; width: {width}px;">
-	<div bind:this={windowBarElement} class="window__bar">
-		<button aria-label="close" class="window__bar__button window__bar__button--close"></button>
-		<button aria-label="minimize" class="window__bar__button window__bar__button--minimize"
-		></button>
-		<button aria-label="fullscreen" class="window__bar__button window__bar__button--fullscreen">
-		</button>
+<div bind:this={windowElement} class="layer" style="height: {height}px; width: {width}px;">
+	<div class="window">
+		<div bind:this={windowBarElement} class="window__bar">
+			<button aria-label="close" class="window__bar__button window__bar__button--close"></button>
+			<button aria-label="minimize" class="window__bar__button window__bar__button--minimize"
+			></button>
+			<button aria-label="fullscreen" class="window__bar__button window__bar__button--fullscreen">
+			</button>
+		</div>
+		<div class="window__content">
+			<h1>{title}</h1>
+			{@render children?.()}
+		</div>
+		<div bind:this={topLeftHandle} class="window__handle window__handle--top-left"></div>
+		<div bind:this={topRightHandle} class="window__handle window__handle--top-right"></div>
+		<div bind:this={rightHandle} class="window__horizontal window__handle--right"></div>
+		<div bind:this={leftHandle} class="window__horizontal window__handle--left"></div>
+		<div bind:this={downHandle} class="window__vertical window__handle--down"></div>
+		<div bind:this={upHandle} class="window__vertical window__handle--top"></div>
+		<div bind:this={bottomLeftHandle} class="window__handle window__handle--bottom-left"></div>
+		<div bind:this={bottomRightHandle} class="window__handle window__handle--bottom-right"></div>
 	</div>
-	<div class="window__content">
-		<h1>{title}</h1>
-		{@render children?.()}
-	</div>
-	<div bind:this={topLeftHandle} class="window__handle window__handle--top-left"></div>
-	<div bind:this={topRightHandle} class="window__handle window__handle--top-right"></div>
-	<div bind:this={bottomLeftHandle} class="window__handle window__handle--bottom-left"></div>
-	<div bind:this={bottomRightHandle} class="window__handle window__handle--bottom-right"></div>
 </div>
 
 <style>
-	.window {
-		position: relative;
-		display: flex;
-		flex-direction: column;
+	.layer {
+		position: absolute;
 		border-radius: 22px;
 		background: var(--window-bg, #fcfcfc);
 		box-shadow: 0 0 12px rgb(80 80 80 / 30%);
 	}
+	.window {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		width: 100%;
+	}
 
 	.window__bar {
-		height: 20px;
+		height: 30px;
 		padding: 8px 12px;
 		cursor: pointer;
 	}
@@ -153,8 +208,20 @@
 
 	.window__handle {
 		position: absolute;
-		height: 20px;
-		width: 20px;
+		height: 10px;
+		width: 10px;
+	}
+
+	.window__horizontal {
+		position: absolute;
+		height: 100%;
+		width: 8px;
+	}
+
+	.window__vertical {
+		position: absolute;
+		width: 100%;
+		height: 8px;
 	}
 
 	.window__handle--top-left {
@@ -165,6 +232,22 @@
 	.window__handle--top-right {
 		top: 0;
 		right: 0;
+	}
+
+	.window__handle--right {
+		right: 0;
+	}
+
+	.window__handle--left {
+		left: 0;
+	}
+
+	.window__handle--down {
+		bottom: 0;
+	}
+
+	.window__handle--top {
+		top: 0;
 	}
 
 	.window__handle--bottom-left {
