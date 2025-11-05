a2dd29d9d3e9533b7b6149eccc14836b7293133e
Joseph-D-Bradshaw <joseph.bradshaw@outlook.com>
2025-11-05 02:44:20 +0100

    add dock icons

--- src/lib/dock/Dock.svelte
+++ src/lib/dock/Dock.svelte
@@ -4,21 +4,19 @@
 </script>
 
 <div class="dock">
-	<button aria-label="app-1" class="dock__app dock__app--green"></button>
+	<button aria-label="Finder" class="dock__app dock__app--finder"></button>
 	<button
 		onclick={() => windowStore.add(note, 'Obsidian')}
-		aria-label="app-2"
-		class="dock__app dock__app--green"
+		aria-label="Obsidian"
+		class="dock__app dock__app--obsidian"
 	></button>
 	<button
 		onclick={() => windowStore.add(note, 'Notes')}
-		aria-label="app-3"
-		class="dock__app dock__app--yellow dock__app--running"
+		aria-label="Notes"
+		class="dock__app dock__app--notes dock__app--running"
 	></button>
-	<button aria-label="app-4" class="dock__app dock__app--blue"></button>
 	<div class="divider"></div>
-	<button aria-label="app-5" class="dock__app dock__app--blue"></button>
-	<button aria-label="app-6" class="dock__app dock__app--blue"></button>
+	<button aria-label="Bin" class="dock__app dock__app--bin"></button>
 </div>
 
 {#snippet note()}
@@ -34,36 +32,35 @@
 		display: flex;
 		gap: 8px;
 		margin: 4px;
-		padding: 16px;
-		border-radius: 32px;
-		background: rgb(255 255 255 / 2%);
-		box-shadow:
-			inset 0 0 12px rgb(255 255 255 / 60%),
-			0 0 12px rgb(0 0 0 / 15%);
+		padding: 6px;
+		border-radius: 20px;
+		background: rgb(151 148 156 / 95%);
+		border: 1px solid rgb(161 158 166 / 95%);
 	}
 
 	.dock__app {
 		width: 60px;
 		height: 60px;
-		border-radius: 20px;
 		cursor: pointer;
 		transform: scale(100%) translateY(0);
 		transition: transform 0.2s ease-in-out;
-		box-shadow:
-			inset 0 8px 8px rgb(255 255 255 / 30%),
-			inset 0 -8px 8px rgb(255 255 255 / 20%);
+		background-size: cover;
 	}
 
-	.dock__app--green {
-		background: hsl(131, 70%, 56%);
+	.dock__app--finder {
+		background-image: url('../assets/FinderIcon.png');
 	}
 
-	.dock__app--blue {
-		background: hsl(205, 98%, 56%);
+	.dock__app--notes {
+		background-image: url('../assets/NoteIcon.png');
 	}
 
-	.dock__app--yellow {
-		background: hsl(49, 98%, 59%);
+	.dock__app--obsidian {
+		background-image: url('../assets/ObsidianIcon.png');
+	}
+
+	.dock__app--bin {
+		background-image: url('../assets/TrashIcon.png');
 	}
 
 	.dock__app--running {
@@ -73,12 +70,12 @@
 	.dock__app--running::after {
 		content: '';
 		position: absolute;
-		bottom: -12px;
+		bottom: -4px;
 		left: 50%;
 		transform: translateX(-50%);
-		width: 6px;
-		height: 6px;
-		background: #142a4b;
+		width: 4px;
+		height: 4px;
+		background: rgb(239 241 240);
 		border-radius: 50%;
 	}
 
@@ -87,17 +84,9 @@
 		transition: transform 0.2s ease-in-out;
 	}
 
-	.dock__app:hover + .dock__app,
-	.dock__app:has(+ .dock__app:hover) {
-		transform: scale(90%);
-		transition: transform 0.2s ease-in-out;
-	}
-
 	.divider {
-		width: 4px;
-		margin: 0 4px;
-		border-radius: 24px;
-		background: rgb(60 119 176 / 60%);
-		box-shadow: inset 0 0 4px rgba(60 119 176);
+		width: 1.5px;
+		margin: 4px 4px;
+		background: rgb(200 194 202);
 	}
 </style>
